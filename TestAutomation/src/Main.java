6a168a205983bfc860e9d8eb3baef00417e6b008
Patrik Szabó <patrikszabo93@gmail.com>
2024-01-20 17:08:11 +0100

    webelementskeysTosend

--- TestAutomation/src/Main.java
+++ TestAutomation/src/Main.java
@@ -1,11 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.idokep.hu/idojaras/Budapest");
+        driver.get("https://www.saucedemo.com/");
+
+
+        try {
+          WebElement  userNameInput = driver.findElement(By.id("user-name"));
+            userNameInput.sendKeys("hello");
+        }catch (NoSuchElementException ex){
+            System.err.println("Cannot find username input field");
+        }
+        Thread.sleep(10000);
         driver.quit();
 
     }
