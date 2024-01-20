71081d55aef703854384418049c3ffe01619712f
Patrik Szabó <patrikszabo93@gmail.com>
2024-01-20 17:43:43 +0100

    usnamepasswordbuttonworked

--- TestAutomation/src/Main.java
+++ TestAutomation/src/Main.java
@@ -14,26 +14,16 @@ public class Main {
         try {
           WebElement  userNameInput = driver.findElement(By.id("user-name"));
             userNameInput.sendKeys("standard_user");
-        }catch (NoSuchElementException ex){
-            System.err.println("Cannot find username input field");
-        }
-        Thread.sleep(3000);
-        try {
+            Thread.sleep(2500);
             WebElement  passwordInput = driver.findElement(By.id("password"));
             passwordInput.sendKeys("secret_sauce");
-        }catch (NoSuchElementException ex){
-            System.err.println("Cannot find username input field");
-        }
-
-        Thread.sleep(3000);
-        try {
+            Thread.sleep(2500);
             WebElement  loginButtonClick = driver.findElement(By.id("login-button"));
             loginButtonClick.click();
+            Thread.sleep(2500);
         }catch (NoSuchElementException ex){
             System.err.println("Cannot find username input field");
         }
-
-        Thread.sleep(5000);
 
         driver.quit();
 
